18bd18c63a776c697bf0d72b9ad6dc4c2ba0fcf4
Mike LaGattuta <mjlagattuta@gmail.com>
2019-01-26 22:38:59 -0500

    Update build to change linked style
    Remove Condensed from any linked styles (for building condensed statics from source)

--- sources/tools/build-static/prep-roman.py
+++ sources/tools/build-static/prep-roman.py
@@ -12,5 +12,7 @@ font = GSFont(filename)
 
 for instance in font.instances:
     instance.name = re.sub(' *Condensed', '', instance.name)
+    if instance.isBold == True:
+    	instance.linkStyle = re.sub(' *Condensed', '', instance.linkStyle)
 
 font.save(filename)
