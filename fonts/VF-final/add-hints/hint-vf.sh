a652e3ce8a9e9a78531f734a9ed3cc0451fc99d0
Mike LaGattuta <mjlagattuta@gmail.com>
2019-01-28 23:49:19 -0500

    Add ttfa control files

---
+++ fonts/VF-final/add-hints/hint-vf.sh
@@ -0,0 +1,2 @@
+ttfautohint-vf --stem-width-mode nnn -m add-hints/Roman-ttfa-ctrl.txt Cabin-Roman-VF.ttf hinted-betas/Cabin-Roman-VF-Hinted.ttf
+ttfautohint-vf --stem-width-mode nnn -m add-hints/Italic-ttfa-ctrl.txt Cabin-Italic-VF.ttf hinted-betas/Cabin-Italic-VF-Hinted.ttf
