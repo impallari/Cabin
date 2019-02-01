06efb3f32db85365b7fa122494390641ebc0d413
Mike LaGattuta <mjlagattuta@gmail.com>
2019-02-01 01:44:57 -0500

    Update control files

--- fonts/VF-linked/add-hints/hint-vf.sh
+++ fonts/VF-linked/add-hints/hint-vf.sh
@@ -1,3 +1,3 @@
-ttfautohint-vf --stem-width-mode nnn -m add-hints/Roman-ttfa-ctrl.txt Cabin-Roman-VF.ttf hinted-betas/Cabin-Roman-VF-Hinted.ttf
-ttfautohint-vf --stem-width-mode nnn -m add-hints/Italic-ttfa-ctrl.txt Cabin-Italic-VF.ttf hinted-betas/Cabin-Italic-VF-Hinted.ttf
-ttfautohint-vf --stem-width-mode nnn -m add-hints/Condensed-ttfa-ctrl.txt CabinCondensed-Roman-VF.ttf hinted-betas/CabinCondensed-Roman-VF-Hinted.ttf
+ttfautohint-vf --stem-width-mode nnn -m add-hints/Roman-ttfa-ctrl.txt Cabin-Roman-VF.ttf hinted/Cabin-Roman-VF-Hinted.ttf
+ttfautohint-vf --stem-width-mode nnn -m add-hints/Italic-ttfa-ctrl.txt Cabin-Italic-VF.ttf hinted/Cabin-Italic-VF-Hinted.ttf
+ttfautohint-vf --stem-width-mode nnn -m add-hints/Condensed-ttfa-ctrl.txt CabinCondensed-Roman-VF.ttf hinted/CabinCondensed-Roman-VF-Hinted.ttf
