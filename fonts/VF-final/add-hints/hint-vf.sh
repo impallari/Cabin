f6ee69c3a496298cb6ced46eaade8ae933f1bbc7
Mike LaGattuta <mjlagattuta@gmail.com>
2019-02-01 01:43:37 -0500

    Removed hinted versions from final VF

--- fonts/VF-final/add-hints/hint-vf.sh
+++
@@ -1,2 +0,0 @@
-ttfautohint-vf --stem-width-mode nnn -m add-hints/Roman-ttfa-ctrl.txt Cabin-Roman-VF.ttf hinted-betas/Cabin-Roman-VF-Hinted.ttf
-ttfautohint-vf --stem-width-mode nnn -m add-hints/Italic-ttfa-ctrl.txt Cabin-Italic-VF.ttf hinted-betas/Cabin-Italic-VF-Hinted.ttf
