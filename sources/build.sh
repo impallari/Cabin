dcda5054e6fb87adfb744b9712b6181216fa903d
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2020-07-14 18:30:14 -0500

    complete VF on build ok

--- sources/build.sh
+++ sources/build.sh
@@ -29,33 +29,33 @@ postprocess_vf $VF_FILEit
 python3 Cabin_stat_table.py $VF_FILE
 
 
-echo "GENERATING TTFs"
-fontmake -m CabinRegular.designspace -i -o ttf --output-dir ../fonts/TTF/ -a
-fontmake -m CabinItalic.designspace -i -o ttf --output-dir ../fonts/TTF/ -a
+# echo "GENERATING TTFs"
+# fontmake -m CabinRegular.designspace -i -o ttf --output-dir ../fonts/TTF/ -a
+# fontmake -m CabinItalic.designspace -i -o ttf --output-dir ../fonts/TTF/ -a
 
-echo "POST PROCESSING TTFs"
-ttfs=$(ls ../fonts/TTF/*.ttf)
-for ttf in $ttfs
-do
-    gftools fix-hinting $ttf;
-    mv "$ttf.fix" $ttf;
+# echo "POST PROCESSING TTFs"
+# ttfs=$(ls ../fonts/TTF/*.ttf)
+# for ttf in $ttfs
+# do
+#     gftools fix-hinting $ttf;
+#     mv "$ttf.fix" $ttf;
 
-    gftools fix-dsig -f $ttf;
-done
-
-
-echo "GENERATING OTFs"
-fontmake -m CabinRegular.designspace -i -o otf --output-dir ../fonts/otf/ -a
-fontmake -m CabinItalic.designspace -i -o otf --output-dir ../fonts/otf/ -a
-
-echo "POST PROCESSING OTFs"
-otfs=$(ls ../fonts/otf/*.otf)
-for otf in $otfs
-do
-    gftools fix-dsig -f $otf;
-done
+#     gftools fix-dsig -f $ttf;
+# done
 
 
-# cleanup
-rm -rf ../fonts/TTF/*gasp*.ttf ../fonts/VF/*gasp*.ttf instance_ufo *.ufo master_ufo/
+# echo "GENERATING OTFs"
+# fontmake -m CabinRegular.designspace -i -o otf --output-dir ../fonts/otf/ -a
+# fontmake -m CabinItalic.designspace -i -o otf --output-dir ../fonts/otf/ -a
+
+# echo "POST PROCESSING OTFs"
+# otfs=$(ls ../fonts/otf/*.otf)
+# for otf in $otfs
+# do
+#     gftools fix-dsig -f $otf;
+# done
+
+
+# # cleanup
+# rm -rf ../fonts/TTF/*gasp*.ttf ../fonts/VF/*gasp*.ttf instance_ufo *.ufo master_ufo/
 
